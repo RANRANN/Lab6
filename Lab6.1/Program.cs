d06f8b13ff95509f54211eeb55b88deb0db5ad9b
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-12 01:31:37 +0300

    решил 6.1

--- Lab6.1/Program.cs
+++ Lab6.1/Program.cs
@@ -10,6 +10,45 @@ namespace Lab6._1
     {
         static void Main(string[] args)
         {
+            string startstring = Console.ReadLine();
+            string[] stringarray = startstring.Split();
+            int max = stringarray[0].Length;
+            int resultstring = 0;
+            foreach (string s in stringarray);
+            {
+                resultstring = stringarray[1].Length;
+                if (max<resultstring)
+                {
+                    max = resultstring;
+                }
+            }
+            /*for (int i=0,stringarray[].Length)
+            {
+                if (a>max)
+                {
+                    max = a;
+                }
+            }
+            Console.WriteLine(max);
+            /*n = stringarray[s.length];
+            for (int i = 0; i < length; i++)
+            {
+                n = (n > s.length) ? n = n : n = s.length;
+            } 
+            n=Math.Max(array[i])
+            Console.WriteLine(n);
+
+            int max = array[0];
+            foreach (int a in array);
+            {
+                if (a>max)
+                max=a;
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+             */
+            Console.WriteLine(max);
+            Console.ReadKey();
         }
     }
 }
