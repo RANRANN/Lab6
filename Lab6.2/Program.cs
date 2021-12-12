d80e2871fb0dab6c8664bbd9a6fd409bb6e110cf
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-12 16:20:20 +0300

    добавил задачу 6.2

---
+++ Lab6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startstring = Console.ReadLine();
+            string S = string.Concat(startstring.ToLower());
+            string s = Convert.ToString(S.Reverse());
+            if (String.Compare(S, s) == 1)
+            {
+                Console.WriteLine("Введённое предложение является полиндромом");
+            }
+            Console.WriteLine("Введённое предложение не является полиндромом");
+            Console.ReadKey();
+
+        }
+    }
+}
